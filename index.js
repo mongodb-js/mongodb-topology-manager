53a565d2d54dc04897f13d1843e40eb79b8889e8
Matt Broadstone <mbroadst@gmail.com>
2017-09-16 13:21:01 -0400

    refactor(es6): remove all es5 related code, as well as babel
    In this major release of the tool we only support node 4+, so we
    no longer have to transpile to es5.

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
-// Polyfill Promise if none exists
-if(!global.Promise) {
-  require('es6-promise').polyfill();
-}
+'use strict';
 
-// Check if we support es6 generators
-try {
-  eval("(function *(){})");
-
-  // Expose all the managers
-  var ConfigServers = require('./lib/config_servers');
-  var Mongos = require('./lib/mongos');
-  var ReplSet = require('./lib/replset');
-  var Server = require('./lib/server');
-  var Sharded = require('./lib/sharded');
-  var Logger = require('./lib/logger');
-} catch(err) {
-  // Load the ES6 polyfills
-  require("babel-polyfill");
-
-  // Load ES5 versions of our managers
-  var ConfigServers = require('./es5/config_servers');
-  var Mongos = require('./es5/mongos');
-  var ReplSet = require('./es5/replset');
-  var Server = require('./es5/server');
-  var Sharded = require('./es5/sharded');
-  var Logger = require('./es5/logger');
-}
-
-// Export all the modules
 module.exports = {
-  ConfigServers: ConfigServers,
-  Mongos: Mongos,
-  ReplSet: ReplSet,
-  Server: Server,
-  Sharded: Sharded,
-  Logger: Logger
-}
+  ConfigServers: require('./lib/config_servers'),
+  Mongos: require('./lib/mongos'),
+  ReplSet: require('./lib/replset'),
+  Server: require('./lib/server'),
+  Sharded: require('./lib/sharded'),
+  Logger: require('./lib/logger')
+};
