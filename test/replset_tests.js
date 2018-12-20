aebc279263e08aa9f5ad4a143fba298ec07edfaf
Addison Elliott <addison.elliott@gmail.com>
2018-12-20 14:00:16 -0600

    feat(*): perform all executable operations in parallel
    * Implement changes
    
    * Update from yielding arrays to using Promise.map for clarity
    
    * Fixed another occurrence of the old yield array method
    
    * Forgot to set timeout test
    
    * Update test to utilize Promises
    
    * Remove nested promise creation, clean up code
    
    * Clean up stop/purge functions
    
    * Forgot to pull changes and update new test to cleanup on exit

--- test/replset_tests.js
+++ test/replset_tests.js
@@ -644,5 +644,82 @@ describe('ReplSet', function() {
         yield topology.stop();
       });
     });
+
+    it('test retrieve number of arbiters, secondary & primary', function() {
+      this.timeout(200000);
+
+      return co(function*() {
+        var ReplSet = require('../').ReplSet;
+
+        // Create new instance
+        var topology = new ReplSet(
+          'mongod',
+          [
+            {
+              // mongod process options
+              options: {
+                bind_ip: 'localhost',
+                port: 31000,
+                dbpath: f('%s/../db/31000', __dirname)
+              }
+            },
+            {
+              // mongod process options
+              options: {
+                bind_ip: 'localhost',
+                port: 31001,
+                dbpath: f('%s/../db/31001', __dirname)
+              }
+            },
+            {
+              // Type of node
+              arbiter: true,
+              // mongod process options
+              options: {
+                bind_ip: 'localhost',
+                port: 31002,
+                dbpath: f('%s/../db/31002', __dirname)
+              }
+            }
+          ],
+          {
+            replSet: 'rs'
+          }
+        );
+        managers.push(topology);
+
+        // Purge any directories
+        yield topology.purge();
+
+        // Start set
+        yield topology.start();
+
+        // Retrieve servers
+        var primary = yield topology.primary();
+        var secondaries = yield topology.secondaries();
+        var passives = yield topology.passives();
+        var arbiters = yield topology.arbiters();
+
+        // Verify primary
+        assert.strictEqual(primary.host, 'localhost');
+        assert.ok(primary.port === 31000 || primary.port === 31001);
+
+        // Verify secondaries
+        assert.strictEqual(secondaries.length, 1);
+        assert.strictEqual(secondaries[0].host, 'localhost');
+        assert.strictEqual(secondaries[0].port, primary.port === 31000 ? 31001 : 31000);
+
+        // Verify passives
+        assert.strictEqual(passives.length, 0);
+
+        // Verify arbiters
+        assert.strictEqual(arbiters.length, 1);
+        assert.strictEqual(arbiters[0].host, 'localhost');
+        assert.strictEqual(arbiters[0].port, 31002);
+
+        // Stop the set
+        yield topology.stop();
+      });
+    });
   });
 });
