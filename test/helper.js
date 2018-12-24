11ae62e88aceaa205ae4435ff5b57f0496f16237
Addison Elliott <addison.elliott@gmail.com>
2018-12-24 09:11:55 -0600

    Remove testing any MongoDB versions under 2.6

--- test/helper.js
+++ test/helper.js
@@ -7,9 +7,11 @@ before(function() {
   // Set default MONGODB_VERSION to be checked against later
   process.env.MONGODB_VERSION = process.env.MONGODB_VERSION || '4.1.6';
 
-  console.log('Installing MongoDB client');
+  console.log('Installing MongoDB server');
 
   return Promise.promisify(mvm.use)({
     version: process.env.MONGODB_VERSION
+  }).then(function() {
+    console.log('Finished installing MongoDB server\n');
   });
 });
