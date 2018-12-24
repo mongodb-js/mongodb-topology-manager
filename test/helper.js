50f0c766794b5fff6b3d53c30657d8a60a3de0d6
Addison Elliott <addison.elliott@gmail.com>
2018-12-24 08:57:00 -0600

    Remove TODO

--- test/helper.js
+++ test/helper.js
@@ -7,7 +7,6 @@ before(function() {
   // Set default MONGODB_VERSION to be checked against later
   process.env.MONGODB_VERSION = process.env.MONGODB_VERSION || '4.1.6';
 
-  // TODO: Decide if I want this always
   console.log('Installing MongoDB client');
 
   return Promise.promisify(mvm.use)({
